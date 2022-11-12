ce4bf1775a822ccfb58005246010e208d6842b06
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-11-12 18:05:32 +0530

    Updates

---
+++ precache-manifest.ffa1dccc5e22f23b50afd9451f166ed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d02805cf4021dc15a74",
+    "url": "/react-todo-firebase-app/static/css/main.1acf1e5f.chunk.css"
+  },
+  {
+    "revision": "1d02805cf4021dc15a74",
+    "url": "/react-todo-firebase-app/static/js/main.1d02805c.chunk.js"
+  },
+  {
+    "revision": "38612289b54661aa30c7",
+    "url": "/react-todo-firebase-app/static/js/1.38612289.chunk.js"
+  },
+  {
+    "revision": "ec92a0220538b7765d3a",
+    "url": "/react-todo-firebase-app/static/js/runtime~main.ec92a022.js"
+  },
+  {
+    "revision": "a4b98e90d77890928b12521dc79e2e63",
+    "url": "/react-todo-firebase-app/index.html"
+  }
+];
